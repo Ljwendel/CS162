29108b9b34b6ac8da246df79407afa2fe33cecfe
Ljwendel <leland.wendel.6235@mail.linnbenton.edu>
2023-11-09 11:54:22 -0800

    Practice update

--- gui_basics/gui_basics.py
+++ gui_basics/gui_basics.py
@@ -1,20 +1,32 @@
 '''Let's make a basic GUI at least show up on the screen.'''
 
 import tkinter as tk
+# import motor_build
 
-import motor_class
+
+def update_label_text():
+    my_label.set()
+
 
 root = tk.Tk()
 root.geometry("300x200")
 root.title("My GUI")
 
 
-my_label = tk.Label(root, text="Hello?")
+my_label_string_var = tk.StringVar(root, "hello?")
+my_label = tk.Label(root, textvariable=my_label_string_var)
 my_label.pack()
+# my_motor = motor_build.Motor()
 
-my_label2 = tk.Label(root, text="Label?")
-my_label2.pack()
+label_button = tk.Button(root, text='update label', command=update_label_text)
+label_button.pack()
 
-my_motor = motor_class.Motor()
-print(f"my_motor: {my_motor}")
+# my_label = tk.Label(root, text=f"Hello?")
+# my_label.pack()
+
+# my_label2 = tk.Label(root, text="Label?")
+# my_label2.pack()
+
+
+# print(f"my_label: {my_label}")
 root.mainloop()
